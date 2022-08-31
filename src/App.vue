dab6aea53bdfb8f2e00cfd1a917a9f6c8b2ffcb8
amekiri13 <admin@amekiri.com>
2022-08-31 16:11:42 +0800

    界面初始化

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
-<h1>Hello</h1>
+<left-menu></left-menu>
+<router-view></router-view>
 </template>
 
 <script>
+import LeftMenu from "./components/LeftMenu";
+import {createRouter,createWebHashHistory} from "vue-router";
+
+const routes = [
+  {path: '/', components: ''}
+];
+
+createRouter({
+  history: createWebHashHistory(),
+  routes
+});
 
 export default {
   name: 'App',
+  components:{
+    LeftMenu
+  },
   methods:{
 
   }
@@ -21,4 +36,7 @@ export default {
 /*  color: #2c3e50;*/
 /*  margin-top: 60px;*/
 /*}*/
+router-view {
+
+}
 </style>
---
+++ src/components/LeftMenu.vue
@@ -0,0 +1,26 @@
+<template>
+<ul class="menu">
+  <li></li>
+</ul>
+</template>
+
+<script>
+export default {
+  name: "LeftMenu"
+}
+</script>
+
+<style scoped>
+.menu {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 80px;
+  background-color: #42b983;
+  height: 100vh;
+  list-style: none;
+}
+.menu>li {
+
+}
+</style>
