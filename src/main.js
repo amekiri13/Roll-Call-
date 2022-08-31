99bdc74d43f70fd91f379f7f648ce3227924b1a0
amekiri13 <admin@amekiri.com>
2022-08-31 19:08:13 +0800

    Vue-Router失败

--- src/main.js
+++ src/main.js
@@ -1,16 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import {createRouter,createWebHashHistory} from "vue-router";
-
-const routes = [
-    {path: '/', components: 'Basic'},
-    {path: '/edit', components: 'Edit'},
-    {path: '/setting', components: 'Setting'}
-];
-
-createRouter({
-    history: createWebHashHistory(),
-    routes
-});
 
 createApp(App).mount('#app')
