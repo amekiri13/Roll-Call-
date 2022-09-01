560ca86f59c28052815f51fd68b9f518ba476e67
amekiri13 <admin@amekiri.com>
2022-09-01 11:19:59 +0800

    使用Font Awesome图标库

--- src/main.js
+++ src/main.js
@@ -2,10 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUserSecret)
+
 const routes  = [
     {path: '/', name: 'home', component: { render() {return "Home"} }},
     {path: '/edit', name: 'edit', component: { render() {return "Edit"} }},
     {path: '/setting', name: 'setting', component: { render() {return "Setting"} }},
+    {path: '/about', name: 'about', component: { render() {return ""}}}
 ]
 
 const router = createRouter({
@@ -13,4 +20,4 @@ const router = createRouter({
     routes
 })
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
