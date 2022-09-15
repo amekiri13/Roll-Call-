73a161cc869660186a8b2f247cd8bbf2b37a6d16
amekiri13 <admin@amekiri.com>
2022-09-15 16:14:42 +0800

    fix:顺利解决左边菜单栏问题

--- src/components/LeftMenu.vue
+++ src/components/LeftMenu.vue
@@ -1,9 +1,9 @@
 <template>
 <ul class="menu">
-  <li @click="changeOption(0)" :class="{'selected' : homeSelected}"><router-link to="/"><img src="../assets/icons/2931150_home_furniture_office_architecture_house_homepage_building.png"></router-link></li>
-  <li @click="changeOption(1)" :class="{'selected' : editSelected}"><router-link to="/edit"><img src="../assets/icons/2931178_creative_edit_pencil_change_draw_design_pen.png"></router-link></li>
-  <li @click="changeOption(2)" :class="{'selected' : settingSelected}"><router-link to="/setting"><img src="../assets/icons/2931152_control_tools_repair_settings_gear_adjust_setup.png"></router-link></li>
-  <li @click="changeOption(3)" style="position: absolute;bottom: 0" :class="{'selected' : aboutSelected}"><router-link to="/about"><img src="../assets/icons/2931180_info_service_about_support_chat_information.png"></router-link></li>
+  <li @click="homeSelected=true;editSelected=false;settingSelected=false;aboutSelected=false" :class="{'selected' : homeSelected}"><router-link to="/"><img src="../assets/icons/2931150_home_furniture_office_architecture_house_homepage_building.png"></router-link></li>
+  <li @click="homeSelected=false;editSelected=true;settingSelected=false;aboutSelected=false" :class="{'selected' : editSelected}"><router-link to="/edit"><img src="../assets/icons/2931178_creative_edit_pencil_change_draw_design_pen.png"></router-link></li>
+  <li @click="homeSelected=false;editSelected=false;settingSelected=true;aboutSelected=false" :class="{'selected' : settingSelected}"><router-link to="/setting"><img src="../assets/icons/2931152_control_tools_repair_settings_gear_adjust_setup.png"></router-link></li>
+  <li @click="homeSelected=false;editSelected=false;settingSelected=false;aboutSelected=true" style="position: absolute;bottom: 0" :class="{'selected' : aboutSelected}"><router-link to="/about"><img src="../assets/icons/2931180_info_service_about_support_chat_information.png"></router-link></li>
 </ul>
 </template>
 
@@ -22,26 +22,7 @@ export default {
 
   },
   methods:{
-    changeOption:(item) => {
-      this.homeSelected = false;
-      this.editSelected = false;
-      this.settingSelected = false;
-      this.aboutSelected = false;
-      switch (item) {
-        case 0:
-          this.homeSelected = true;
-          break;
-        case 1:
-          this.editSelected = true;
-          break;
-        case 2:
-          this.settingSelected = true;
-          break;
-        case 3:
-          this.aboutSelected = true;
-          break;
-      }
-    }
+
   }
 }
 </script>
